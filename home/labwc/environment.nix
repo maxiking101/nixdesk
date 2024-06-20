963d6c000f59f866cad9a0159f2bb819eaeb056e
Max Rabe <max.rabe@sap.com>
2024-06-20 11:48:38 +0200

    change nvidia env vars

--- home/labwc/environment.nix
+++ home/labwc/environment.nix
@@ -7,7 +7,7 @@
       "MOZ_ENABLE_WAYLAND" = 1;
       "WLR_NO_HARDWARE_CURSORS"=1;
       "NIXOS_OZONE_WL" = 1;
-      "GBM_BACKEND" = "nvidia";
+      "GBM_BACKEND" = "nvidia-drm";
       "LIBVA_DRIVER_NAME" = "nvidia";
       "__GLX_VENDOR_LIBRARY_NAME" = "nvidia";
       "XDG_SESSION_TYPE" = "wayland";
